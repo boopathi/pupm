9abcdb7edf05938d8b267c2a8f49041cc3ce0ada
Boopathi Rajaa <me@boopathi.in>
2012-07-19 16:56:11 +0530

    made the init files for apache and nginx generic and working

--- apache/manifests/init.pp
+++ apache/manifests/init.pp
@@ -1,17 +1,26 @@
 class apache {
-  package { 'httpd':
-      ensure => installed
-    }
-
-  file { '/etc/httpd/conf/httpd.conf':
-      mode => 644,
-      owner => root,
-      group => root,
-      source => 'puppet:///modules/apache/httpd.conf'
-    }
-
-  service { 'httpd' :
-      ensure => running
-    }
-
+	include apacheInstall
+	include apacheConfig
+	include apacheStart
+	class { "apacheConfig": require => Class["apacheInstall"] }
+	class { "apacheStart": require => Class["apacheConfig"] }
 }
+class apacheInstall {
+	package { 'httpd':
+		ensure => installed
+	}
+}
+class apacheConfig {
+	file { '/etc/httpd/conf/httpd.conf':
+		mode=>644,
+		owner=>root,
+		group=>root,
+		source=>'puppet:///modules/apache/httpd.conf'
+	}
+}
+class apacheStart {
+	service { 'httpd':
+		ensure=>running
+	}
+}
+
--- nginx/manifests/init.pp
+++ nginx/manifests/init.pp
@@ -1,17 +1,25 @@
 class nginx {
+	include nginxInstall
+	include nginxConfigure
+	include nginxStart
+	class { "nginxConfigure" : require=>Class["nginxInstall"] }
+	class { "nginxStart" : require=>Class["nginxConfigure"] }
+}
+class nginxInstall {
   package { 'nginx':
       ensure => installed
     }
 
+class nginxConfigure {
   file { '/etc/nginx/nginx.conf':
       mode => 644,
       owner => root,
       group => root,
       source => 'puppet:///modules/nginx/httpd.conf'
     }
-
+}
+class nginxStart {
   service { 'nginx' :
       ensure => running
     }
-
 }
