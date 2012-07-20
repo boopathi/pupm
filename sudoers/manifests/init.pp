fa731f30809501b0cb97bd3a39c49ec1b4ddc9c4
Boopathi Rajaa <me@boopathi.in>
2012-07-20 12:54:01 +0530

    included sudoers to require sudo

--- sudoers/manifests/init.pp
+++ sudoers/manifests/init.pp
@@ -1,9 +1,13 @@
 class sudoers {
+	package {"sudo":
+		ensure=>latest
+	}
 	file {"/etc/sudoers" :
 		mode=>440,
 		user=>root,
 		group=>root,
-		source=>'puppet:///modules/sudoers/sudoers'
+		source=>'puppet:///modules/sudoers/sudoers',
+		require=>Package['sudo']
 	}
 }
 
