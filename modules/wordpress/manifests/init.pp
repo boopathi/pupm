91be0ffa1c32cd0d60cf58b71203f09be48cf993
Abhijeet <me@abhijeetr.com>
2012-07-20 15:45:17 +0530

    Added wordpress install class

--- modules/wordpress/manifests/init.pp
+++ modules/wordpress/manifests/init.pp
@@ -1,9 +1,17 @@
 class wordpress {
 	include addWordpressRepo
+    include installWordpress
 	}
 class addWordpressRepo {
 	file { '/etc/yum.repos.d/wordpress.repo':
 		source => 'puppet:///modules/wordpress/wordpress.repo'
 	}
-
 }	
+
+class installWordpress {
+    package { 'wordpress-custom':
+        ensure => installed,
+    }
+}
+
+Class["installWordpress"] -> Class["addWordpressRepo"]
