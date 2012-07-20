0111878a4afcf88786bf01d727c3a5a73066a7c1
Boopathi Rajaa <me@boopathi.in>
2012-07-20 14:24:15 +0530

    added puppet host

--- hosts/manifests/init.pp
+++ hosts/manifests/init.pp
@@ -7,6 +7,7 @@ class etchosts {
 	}
 }
 class hosts {
+	host { 'puppet': ip=>'172.16.143.171' }
 	host { 'ga': ip=>'172.16.143.171'}
 	host { 'gb': ip=>"172.16.143.172" }
 	host { 'gc': ip=>"172.16.143.173" }
