e18471922a77a9eb3df59f7c8ba7a408bd836e2c
Boopathi Rajaa <me@boopathi.in>
2012-07-20 14:14:13 +0530

    test

--- hosts/manifests/init.pp
+++ hosts/manifests/init.pp
@@ -7,7 +7,10 @@ class etc_hosts {
 	}
 }
 class hosts {
-	host { 'ga': ip=>"172.16.143.171" }
+	host { 'ga': 
+		ip=>'172.16.143.171'
+	}
+	
 	host { 'gb': ip=>"172.16.143.172" }
 	host { 'gc': ip=>"172.16.143.173" }
 	host { 'gd': ip=>"172.16.143.174" }
