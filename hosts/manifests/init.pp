050e582e53bf0039d3eaaecbe0d90aafc70e86d9
Boopathi Rajaa <me@boopathi.in>
2012-07-20 14:19:21 +0530

    init.pp

--- hosts/manifests/init.pp
+++ hosts/manifests/init.pp
@@ -3,14 +3,11 @@ class hosts {
 		mode=>644,
 		owner=>root,
 		group=>root,
-		source=>'puppet:///modules/hosts/hosts"
+		source=>'puppet:///modules/hosts/hosts'
 	}
 }
 class etc_hosts {
-	host { 'ga': 
-		ip=>'172.16.143.171'
-	}
-	
+	host { 'ga': ip=>'172.16.143.171'}
 	host { 'gb': ip=>"172.16.143.172" }
 	host { 'gc': ip=>"172.16.143.173" }
 	host { 'gd': ip=>"172.16.143.174" }
