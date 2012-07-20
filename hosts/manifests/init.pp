f9ef027c31c359e149c832752d41c8c00ca6afa6
Boopathi Rajaa <me@boopathi.in>
2012-07-20 13:12:07 +0530

    another way of doing hosts in etc

--- hosts/manifests/init.pp
+++ hosts/manifests/init.pp
@@ -6,4 +6,10 @@ class etc_hosts {
 		source=>'puppet:///modules/hosts/hosts"
 	}
 }
-
+class hosts {
+	host { 'ga': ip=>172.16.143.171 }
+	host { 'gb': ip=>172.16.143.172 }
+	host { 'gc': ip=>172.16.143.173 }
+	host { 'gd': ip=>172.16.143.174 }
+	host { 'ge': ip=>172.16.143.175 }
+}
