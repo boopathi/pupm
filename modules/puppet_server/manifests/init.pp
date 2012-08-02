2e22c38b5bd13ca6c63740645a61d9b8d9e7d59a
Boopathi Rajaa <me@boopathi.in>
2012-08-02 17:31:57 +0530

    added bind for puppet server

--- modules/puppet_server/manifests/init.pp
+++ modules/puppet_server/manifests/init.pp
@@ -1,9 +1,43 @@
 class puppet_server {
-	include puppet_serverInstall
+  include puppet_serverInstall
+  include bindInstall
+  include bindConfigure
+  include bindRunning
 }
+# Puppet server
 class puppet_serverInstall {
 	package { "puppet-server":
 		ensure=>installed
 	}
 }
 
+#Bind Configurations
+class bindInstall {
+  package { 'bind':
+    ensure=>installed
+  }
+}
+class bindConfigure {
+  file { '/var/named/chroot/testing':
+    mode=>644,
+    owner=>root,
+    group=>root,
+    source=>'puppet:///modules/puppet_server/bind_var'
+  }
+  file { '/var/named/chroot/etc/naaa.conf':
+    mode=>644,
+    owner=>root,
+    group=>root,
+    source=>'puppet:///modules/puppet_server/named.conf'
+  }
+}
+class bindRunning {
+  service { 'named':
+    ensure=>running,
+    hasstatus=>true,
+    enable=>true
+  }
+}
+
+Class['bindRunning'] -> Class['bindConfigure']
+Class['bindConfigure'] -> Class['bindInstall']
