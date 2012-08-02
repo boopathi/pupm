dada54f8f7d3fdd862f75163beaef01e8f0df717
Boopathi Rajaa <me@boopathi.in>
2012-08-02 22:58:56 +0530

    site.pp to x.g1.foo

--- manifests/site.pp
+++ manifests/site.pp
@@ -1,24 +1,31 @@
 #node /^group1_2$/ {
-class initial {
-	include hosts
-	include users
-	include puppet_client
-	include wordpress
-}
-node 'group1_2.internal.directi.com' {
-	include initial
-	include tomcat
-	include nginx
-	include php
-	include bind
-}
+# class initial {
+# 	include hosts
+# 	include users
+# 	include puppet_client
+# 	include wordpress
+# }
+# node 'group1_2.internal.directi.com' {
+# 	include initial
+# 	include tomcat
+# 	include nginx
+# 	include php
+# 	include bind
+# }
 
-node /^group1_3.internal.directi.com$/ {
-	include initial
-	include apache
-	include tomcat
-	include php
-	include nagios
+# node /^group1_3.internal.directi.com$/ {
+# 	include initial
+# 	include apache
+# 	include tomcat
+# 	include php
+# 	include nagios
+# }
+node 'x.g1.foo' {
+  include apache
+  include tomcat
+  include tomcat::apache_conf
+  include php
+  include wordpress
 }
 node 'dbm.g1.foo' {
 	include mysql
