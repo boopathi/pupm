04219058904b579ad269455e9c77bd5505bb8243
Abhijeet <me@abhijeetr.com>
2012-08-02 12:26:06 +0530

    Changed the php module from 5.1 to 5.3

--- modules/php/manifests/init.pp
+++ modules/php/manifests/init.pp
@@ -1,11 +1,32 @@
 class php {
-	include phpInstall
+    include phpRemoveUnwanted
+    include phpInstall
+}
+
+class phpRemoveUnwanted
+{
+    package {"php":
+        ensure => absent;
+    }
+    package {"php-cli":
+        ensure => absent;
+    }
+    package {"php-common":
+        ensure => absent;
+    }
+    package {"php-ldap":
+        ensure => absent;
+    }
 }
 
 class phpInstall {
-	package {"php":
-		ensure => installed
-	}
+    package {"php53":
+        ensure => installed
+    }
+    package {"php53-common":
+        ensure => installed
+    }
+    package {"php53-mysql":
+        ensure => installed
+    }
 }
-
-
