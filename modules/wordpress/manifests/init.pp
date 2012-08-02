b64d52865be24f92bbc971c1e03bd4c3ca4a758a
Boopathi Rajaa <me@boopathi.in>
2012-08-02 23:32:30 +0530

    Wordpress modifications

--- modules/wordpress/manifests/init.pp
+++ modules/wordpress/manifests/init.pp
@@ -1,28 +1,50 @@
 class wordpress {
-	include addWordpressRepo
-    include installWordpress
-	include configureHttpdForWordpress
+
+  file { '/etc/yum.repos.d/wordpress.repo':
+    source => 'puppet:///modules/wordpress/wordpress.repo'
+  }
+  
+  package {'wordpress-custom':
+    ensure=>installed,
+    require=>File['/etc/yum.repos.d/wordpress.repo']
+  }
+
+  file { '/etc/httpd/conf.d/wordpress.conf':
+    source => 'puppet:///modules/wordpress/wordpress.conf',
+    require=>Package['wordpress-custom']
+  }
+  file { '/opt/wordpress/wp-config.php':
+    require=>Package['wordpress-custom'],
+    source => 'puppet:///modules/wordpress/wp-config.php'
+  }	
 }
 
-class addWordpressRepo {
-	file { '/etc/yum.repos.d/wordpress.repo':
-		source => 'puppet:///modules/wordpress/wordpress.repo'
-	}
-}	
 
-class installWordpress {
-    package { 'wordpress-custom':
-        ensure => installed,
-    }
-}
+# class wordpress {
+# 	include addWordpressRepo
+#     include installWordpress
+# 	include configureHttpdForWordpress
+# }
 
-class configureHttpdForWordpress {
-	file { '/etc/httpd/conf.d/wordpress.conf':
-		source => 'puppet:///modules/wordpress/wordpress.conf'
-	}
-	file { '/opt/wordpress/wp-config.php':
-		source => 'puppet:///modules/wordpress/wp-config.php'
-	}
-}
-Class["installWordpress"] -> Class["addWordpressRepo"]
-Class["configureHttpdForWordpress"] -> Class["installWordpress"]
+# class addWordpressRepo {
+# 	file { '/etc/yum.repos.d/wordpress.repo':
+# 		source => 'puppet:///modules/wordpress/wordpress.repo'
+# 	}
+# }	
+
+# class installWordpress {
+#     package { 'wordpress-custom':
+#         ensure => installed,
+#     }
+# }
+
+# class configureHttpdForWordpress {
+# 	file { '/etc/httpd/conf.d/wordpress.conf':
+# 		source => 'puppet:///modules/wordpress/wordpress.conf'
+# 	}
+# 	file { '/opt/wordpress/wp-config.php':
+# 		source => 'puppet:///modules/wordpress/wp-config.php'
+# 	}
+# }
+# Class["installWordpress"] -> Class["addWordpressRepo"]
+# Class["configureHttpdForWordpress"] -> Class["installWordpress"]
