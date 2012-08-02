cc0f9038669fc75a51e04044f94b9132ed0e99b0
Boopathi Rajaa <me@boopathi.in>
2012-08-02 20:21:28 +0530

    new bind config

--- modules/puppet_server/manifests/init.pp
+++ modules/puppet_server/manifests/init.pp
@@ -11,39 +11,67 @@ class puppet_serverInstall {
 	}
 }
 
-#Bind Configurations
-class bindInstall {
-  package { 'bind':
-    ensure=>present
+#New bind config
+class bind {
+  package {
+    'bind': ensure=>installed;
+    'bind-chroot': require=>Package['bind'];
   }
-  package {'bind-chroot':
-    ensure=>installed,
-    require=>Package['bind']
-  }
-}
-
-class bindConfigure {
   File {
     mode=>644,
     owner=>root,
-    group=>root
+    group=>root,
+    notify=>Service['named'],
+    require=>Package['bind-chroot']
   }
-  file { '/var/named/chroot/var/named':
+  file { '/var/named/chroot//var/named':
     recurse=>true,
     source=>'puppet:///modules/puppet_server/bindvar'
   }
-  file { '/var/named/chroot/etc/named.conf':
+  file { '/var/named/chroot//etc/named.conf':
     source=>'puppet:///modules/puppet_server/named.conf'
   }
-}
-
-class bindRunning {
-  service { 'named':
+  service{ 'named':
     ensure=>running,
     hasstatus=>true,
-    enable=>true
+    enable=>true,
+    require=>[ Package['bind-chroot'], File['/var/named/chroot//etc/named.conf'] ]
   }
 }
+#Bind Configurations
+# class bindInstall {
+#   package { 'bind':
+#     ensure=>present
+#   }
+#   package {'bind-chroot':
+#     ensure=>installed,
+#     require=>Package['bind']
+#   }
+# }
 
-Class['bindRunning'] -> Class['bindConfigure']
-Class['bindConfigure'] -> Class['bindInstall']
+# class bindConfigure {
+#   File {
+#     mode=>644,
+#     owner=>root,
+#     group=>root,
+#     notify=>Service['named']
+#   }
+#   file { '/var/named/chroot//var/named':
+#     recurse=>true,
+#     source=>'puppet:///modules/puppet_server/bindvar'
+#   }
+#   file { '/var/named/chroot//etc/named.conf':
+#     source=>'puppet:///modules/puppet_server/named.conf'
+#   }
+# }
+
+# class bindRunning {
+#   service { 'named':
+#     ensure=>running,
+#     hasstatus=>true,
+#     enable=>true
+#   }
+# }
+
+# Class['bindRunning'] -> Class['bindConfigure']
+# Class['bindConfigure'] -> Class['bindInstall']
