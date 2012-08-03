6afe8d07f39236aeffc2205d5e1dbc768366fd43
root <root@localhost.localdomain>
2012-08-03 13:39:03 +0530

    Added the postfix and dovecot part with requied main.cf and dovecot.conf

--- modules/postfix/manifests/init.pp
+++ modules/postfix/manifests/init.pp
@@ -1,18 +1,32 @@
 class postfix {
-  package { 'postfix':
-    ensure => installed,
-  }
-  file { '/etc/postfix/main.cf':
-    mode => 644,
-    owner => root,
-    group => root,
-    source => 'puppet:///modules/postfix/main.cf',
-    require=>Package['postfix'],
-  }
-  service { 'postfix' :
-    ensure => running,
-    enable=>true,
-    hasstatus=>true,
-    require=>[Package['postfix'], File['/etc/postfix/main.cf']],
-  }
+	package { 'postfix': ensure => installed }
+	package { 'dovecot': ensure => installed }
+	package { 'sendmail': ensure => absent }
+
+	file { '/etc/postfix/main.cf':
+		mode => 644,
+		ownner => root,
+		group => root,
+		source => 'puppet:///modules/postfix/main.cf',
+		require=>[Package['postfix'], Package['sendmail']],
+  	}
+	file { '/etc/dovecot.conf':
+		mode => 644,
+		ownner => root,
+		group => root,
+		source => 'puppet:///modules/postfix/dovecot.conf',
+		require=>Package['dovecot'], Package['sendmail']],
+  	}	
+	service { 'postfix':
+		ensure => running,
+    		enable=>true,
+    		hasstatus=>true,
+    		require=>[Package['postfix'], Package['sendmail'], File['/etc/postfix/main.cf']],
+  	}
+	service { 'dovecot':
+		ensure => running,
+    		enable=>true,
+    		hasstatus=>true,
+    		require=>[Package['dovecot'], Package['sendmail'], File['/etc/dovecot.conf']],
+	}
 }
