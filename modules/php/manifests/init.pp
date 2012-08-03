552012474511fb0dc44a2742ab67a0f697b055d2
Boopathi Rajaa <me@boopathi.in>
2012-08-03 21:08:05 +0530

    Added proper selection statements for service notifications

--- modules/php/manifests/init.pp
+++ modules/php/manifests/init.pp
@@ -10,7 +10,14 @@ class phpRemoveUnwanted
 
 class phpInstall {
   package {
-    ['php53', 'php53-common']: ensure=>installed;
-    'php53-mysql': ensure=>installed, notify=>Service['httpd']
+    ['php53', 'php53-common']: ensure=>installed
   }
+  if defined( Service['httpd'] ) {
+    package { 'php53-mysql': ensure=>installed, notify=>Service['httpd'] }
+    } elsif defined ( Service['nginx'] ) {
+    package { 'php53-mysql': ensure=>installed, notify=>Service['nginx'] }
+    } else {
+    package { 'php53-mysql': ensure=>installed }
+    }
 }
+~                    
