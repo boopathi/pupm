2d0db3a65d22015428d9d10be37f6b46b80e9d2a
Boopathi Rajaa <me@boopathi.in>
2012-08-08 14:20:57 +0530

    testing cluster

--- manifests/site.pp
+++ manifests/site.pp
@@ -1,34 +1,38 @@
-#server1
-node /^g1.foo$/ {
-  include postfix
-  include puppet_server
-  include puppet_server::bind
-  include puppet_server::cron
+node /g1.foo$/ {
+  include cluster
 }
 
-#server2
-node /^x.g1.foo$/ {
-  include apache
-  include tomcat
-  include tomcat::apache_conf
-  include php
-  include wordpress
-}
+# #server1
+# node /^g1.foo$/ {
+#   include postfix
+#   include puppet_server
+#   include puppet_server::bind
+#   include puppet_server::cron
+# }
 
-#server3
-node /^y.g1.foo$/ {
-  include nginx
-  include nginx::php
-  include php
-  #include fastcgi
-}
+# #server2
+# node /^x.g1.foo$/ {
+#   include apache
+#   include tomcat
+#   include tomcat::apache_conf
+#   include php
+#   include wordpress
+# }
 
-#server4
-node /^dbm.g1.foo$/ {
-  include mysql_master
-}
+# #server3
+# node /^y.g1.foo$/ {
+#   include nginx
+#   include nginx::php
+#   include php
+#   #include fastcgi
+# }
 
-#server 5
-node /^dbs.g1.foo$/ {
-  include mysql_slave
-}
+# #server4
+# node /^dbm.g1.foo$/ {
+#   include mysql_master
+# }
+
+# #server 5
+# node /^dbs.g1.foo$/ {
+#   include mysql_slave
+# }
