c8f843c614b53078c65ad81e884e580aa403f793
Boopathi Rajaa <me@boopathi.in>
2012-08-08 19:10:59 +0530

    Added cluster

---
+++ modules/cluster/manifests/init.pp
@@ -0,0 +1,42 @@
+class wordpress {
+  $repofile = '/etc/yum.repos.d/pacemaker.repo'
+  $haconf = '/etc/ha.d/ha.cf'
+  $authkeys = '/etc/ha.d/authkeys'
+
+  #one
+  file { $repofile
+    source => 'puppet:///modules/cluster/pacemaker.repo'
+  }
+
+  #install required packages
+  service {
+    ['pacemaker', 'corosync', 'heartbeat']:
+      ensure=>installed,
+      require=>File[$repofile],
+  }
+
+  #copy authkeys and ha.cf
+  file {
+    $haconf :
+      require=>[Service['pacemaker'], Server['corosync'], Service['heartbeat']],
+      source=>'puppet:///modules/cluster/ha.cf' ;
+    $authkeys :
+      source=>'puppet:///modules/cluster/authkeys',
+      require=>[Service['pacemaker'], Server['corosync'], Service['heartbeat']],
+      mode=>600,
+      owner=>root,
+      group=>root,
+  }
+
+  #disable selinux
+  exec { 'disable_selinux':
+    command => 'echo 0 > /selinux/enforce'
+  }
+
+  #start the service
+  service { 'heartbeat':
+    ensure=>running,
+    require=> Exec['disable_selinux'],
+  }
+  
+}
